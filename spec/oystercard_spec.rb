0a0a9e9c7b4571d4df72616eb55122812c0caadc
Eds <steadyede@live.co.uk>
2021-02-08 17:04:15 +0000

    Added initial balance, top up feature

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,21 @@
+require "oystercard"
+
 describe Oystercard do
+
+  it { is_expected.to respond_to(:balance) } 
+
+  describe '#initialize' do
+    it "sets an initial balance of 0 " do
+      oyster = Oystercard.new
+      expect(oyster.balance).to eq 0
+    end
+  end
+
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up) }
+    it "allows user to add money to card" do
+      oyster = Oystercard.new
+      expect(oyster.top_up(5)).to eq 5
+    end
+  end
 end

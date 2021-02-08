e9574d01dac6523af4e3e5b3dafa0b0712c764b9
Eds <steadyede@live.co.uk>
2021-02-08 22:33:40 +0000

    Deduct feature

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,20 @@
 class Oystercard
   attr_reader :balance
+  MAX_LIMIT = 90
 
   def initialize(balance = 0)
     @balance = balance
   end
   
   def top_up(amount)
+    raise "Balance cannot exceed #{MAX_LIMIT}" if @balance + amount > MAX_LIMIT
+
     @balance += amount
   end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,7 +5,7 @@ describe Oystercard do
   it { is_expected.to respond_to(:balance) } 
 
   describe '#initialize' do
-    it "sets an initial balance of 0 " do
+    it "sets an initial balance of 0" do
       oyster = Oystercard.new
       expect(oyster.balance).to eq 0
     end
@@ -17,5 +17,19 @@ describe Oystercard do
       oyster = Oystercard.new
       expect(oyster.top_up(5)).to eq 5
     end
+    it "sets a maximum limit on the card" do
+      max_limit = Oystercard::MAX_LIMIT
+      subject.top_up(max_limit)
+      expect{ subject.top_up(1) }.to raise_error "Balance cannot exceed #{max_limit}"
+    end
   end
+
+  describe '#deduct' do
+    it "deducts a fare from card" do
+      subject.top_up(20)
+      expect(subject.deduct(10)).to eq 10
+    end
+  end
+
+
 end
